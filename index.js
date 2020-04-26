7419032dc56c231c5739a2e56ca59c23ab062e44
Scott Fraser <scottfraserdev@gmail.com>
2020-04-26 11:25:42 -0700

    copypasta

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(app.use(express.static("client/build")));
+app.use(express.static("client/build"));
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
