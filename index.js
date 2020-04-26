c35b9325194cc5de1838d80725ee3c2a34d75cf4
Scott Fraser <scottfraserdev@gmail.com>
2020-04-26 12:28:47 -0700

    another try

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const axios = require("axios");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
