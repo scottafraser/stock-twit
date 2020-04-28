73f71dcc4b9ada8b3b4da0fa40648ba1ae968353
Scott Fraser <scottfraserdev@gmail.com>
2020-04-28 02:08:50 -0700

    parse links

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -8,6 +8,7 @@ import Typography from "@material-ui/core/Typography";
 import moment from "moment";
 import Avatar from "@material-ui/core/Avatar";
 import FavoriteIcon from "@material-ui/icons/Favorite";
+import parse from "html-react-parser";
 
 const useStyles = makeStyles({
   root: {
@@ -23,6 +24,19 @@ export default function SimpleCard({ message }) {
   const classes = useStyles();
   const time = moment(message.created_at).format("h:mma, MMM Do");
 
+  const createTextLinks = (content) => {
+    var exp_match = /(\b(https?|):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+    var element_content = content.replace(exp_match, "<a href='$1'>$1</a>");
+    var new_exp_match = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    var new_content = element_content.replace(
+      new_exp_match,
+      '$1<a target="_blank" href="http://$2">$2</a>'
+    );
+    return new_content;
+  };
+
+  const body = createTextLinks(message.body);
+
   return (
     <Card className={classes.root}>
       <CardHeader
@@ -37,7 +51,7 @@ export default function SimpleCard({ message }) {
         subheader={time}
       />
       <CardContent style={{ paddingTop: "0px" }}>
-        <Typography component="h6">{message.body}</Typography>
+        <Typography component="p">{parse(`${body}`)}</Typography>
       </CardContent>
       <CardContent>
         <Badge
